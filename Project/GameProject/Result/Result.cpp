15a2ff3e1d36c75f3264ea32108bdcab6179f2ed
hagumu1029 <24051906@d.anabuki-college.net>
2026-09-17 11:34:01 +0900

    asasasas

--- Project/GameProject/Result/Result.cpp
+++ Project/GameProject/Result/Result.cpp
@@ -1,23 +1,21 @@
-#pragma once
-#include "../Base/Base.h"
+#include "Result.h"
+#include "Title/Title.h"
 
-class Result : public Base {
-public:
-	static int p;
-	static int g1;
-	static int g2;
-	static int m;
-	CImage m_perfect;
-	CImage m_great;
-	CImage m_good;
-	CImage m_miss;
-	CImage m_img;
-	CImage m_screen;
-	CImage m_Onp;
-	CImage m_eva;
-	int result;
-	int m_cnt;
-	Result(const CVector2D& p);
-	void Update();
-	void Draw();
-};
+Result::Result(const CVector2D& p):Base(eType_Scene)
+{
+	m_img = COPY_RESOURCE("", CImage);
+	m_img.SetSize(1920, 1080);
+}
+
+void Result::Update()
+{
+	if (m_cnt++ > 60 && PUSH(CInput::eButton5)) {
+		KillAll();
+		new Title();
+	}
+}
+
+void Result::Draw()
+{
+	
+}
--- Project/GameProject/Result/Result.h
+++ Project/GameProject/Result/Result.h
@@ -1,1 +1,12 @@
 #pragma once
+#include "../Base/Base.h"
+
+class Result : public Base {
+public:
+	CImage m_img;
+	int result;
+	int m_cnt;
+	Result(const CVector2D& p);
+	void Update();
+	void Draw();
+};
