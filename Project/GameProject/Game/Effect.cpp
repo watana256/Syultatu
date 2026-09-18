e2001d6f3ea8b193e5b54c4d9cceed68d0048a44
watana256 <26051910@d.anabuki-college.net>
2026-09-18 10:17:31 +0900

    a

--- Project/GameProject/Game/Effect.cpp
+++ Project/GameProject/Game/Effect.cpp
@@ -17,7 +17,7 @@ Effect::Effect(const CVector2D& pos) :Base(eType_Effect)
     m_pos = pos;
     m_img.SetSize(128, 128);
     m_img.SetCenter(64, 64);
-    //////////////m_img.ChangeAnimation(0, false);
+    m_img.ChangeAnimation(0, false);
 
 }
 void Effect::Update()
